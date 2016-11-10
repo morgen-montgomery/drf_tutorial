28f96e8fc6d47bced9982df226c4fa78adb3df72
Morgen Fett <mfett004@gmail.com>
2016-11-10 15:12:13 -0800

    utilize generic mix in views for SnippetDetail

--- snippets/views.py
+++ snippets/views.py
@@ -9,24 +9,8 @@ class SnippetList(generics.ListCreateAPIView):
     serializer_class = SnippetSerializer
 
 
-# creats a class 'SnippetDetail' and pass in Retrieve, Update, Destroy, and Generic mixins
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    # get all snippet objects as a list and set them to 'queryset'
-    queryset = Snippet.objects.all()
-    # serialize things
+# create class 'SnippetDetail' and pass in a generic that will retrieve, update and
+# destroy a specific object, and generate a generic API view
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippets.objects.all()
     serializer_class = SnippetSerializer
-
-    # if get is called, get and return the snippet of serialized data
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    # if put is called, update the snippet of serialized data
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    # if delete is called, find and destroy the snippet of serialized data
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
