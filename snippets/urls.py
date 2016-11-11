fbe6add218e4bc4c939870c63d1d2717b526d2c5
Morgen Fett <mfett004@gmail.com>
2016-11-11 11:56:28 -0800

    more things

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,6 +7,8 @@ urlpatterns = {
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^snippets/$', views.SnippetList.as_view()),
     url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^snippte')
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
