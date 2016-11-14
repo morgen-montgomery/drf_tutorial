ecd2db5ed68db801464305b6ffb6b494b3880641
Morgen Fett <mfett004@gmail.com>
2016-11-14 06:36:51 -0800

    get rid of api_root

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
-from snippets.views import SnippetViewSet, UserViewSet, api_root
+from snippets.views import SnippetViewSet, UserViewSet
 from rest_framework import renderers
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
@@ -18,7 +18,7 @@ router.register(r'users', views.UserViewSet)
 # API urls now determined automatically by using a router
 # Login / logout views for browsable API
 urlpatterns = [
-url('^schema/$', schema_view)
+    url('^schema/$', schema_view),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
