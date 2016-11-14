9afe60faee8b9bb123b29a26ef43d1af2bf21757
Morgen Fett <mfett004@gmail.com>
2016-11-14 05:27:41 -0800

    lots of things

--- snippets/views.py
+++ snippets/views.py
@@ -8,7 +8,7 @@ from snippets.serializers import SnippetSerializer
 from rest_framework import generics
 
 from rest_framework.decorators import api_view
-from rest_framework impost renderers
+from rest_framework import renderers
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -68,3 +68,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 10
+}
